3c1e6dc5c54065cbf2fcfade954278f794e5aa5a
Aleksandr1505RY <sryabenko53@gmail.com>
2022-09-04 17:09:30 +0300

    ДЗ №17

--- main.js
+++ main.js
@@ -5,11 +5,9 @@ let prevIndicator = null;
 
 function createContainer() {
   elem = document.createElement("div");
-
   elem.setAttribute("id", "carousel");
   elem.setAttribute("class", "carousel");
   document.querySelector("body").appendChild(elem);
-
   container = document.querySelector("#carousel");
 }
 
@@ -20,7 +18,6 @@ function createSlides(n) {
   for (i = 0; i < n; i++) {
     let slideItem = document.createElement("li");
     let slideLink = document.createElement("a");
-
     slideItem.setAttribute(
       "class",
       i === 0 ? "slides__item active" : "slides__item"
@@ -36,10 +33,8 @@ function createSlides(n) {
 function createIndicators(n) {
   indicatorsContainer = document.createElement("div");
   indicatorsContainer.setAttribute("class", "indicators");
-
   for (i = 0; i < n; i++) {
     let indicatorsItem = document.createElement("span");
-
     indicatorsItem.setAttribute(
       "class",
       i === 0 ? "indicators__item active" : "indicators__item"
@@ -47,10 +42,8 @@ function createIndicators(n) {
     indicatorsItem.setAttribute("data-slide-to", i);
     indicatorsContainer.appendChild(indicatorsItem);
   }
-
   container.appendChild(indicatorsContainer);
 }
-
 function createControls() {
   controlsContainer = document.createElement("div");
   controlsContainer.setAttribute("class", "controls");
@@ -60,7 +53,6 @@ function createControls() {
     let controlIcon = document.createElement("i");
     const defItemClass = "controls__item";
     const defIconClass = "fas";
-
     switch (i) {
       case 0:
         controlItem.setAttribute("class", `${defItemClass} controls__prev`);
@@ -80,7 +72,6 @@ function createControls() {
   }
   container.appendChild(controlsContainer);
 }
-
 function createStyle() {
   styleContainer = document.createElement("style");
   let styleCode = `
@@ -99,29 +90,21 @@ function createStyle() {
       margin: 5px;
       border-radius: 10px;
     }`;
-
   styleContainer.innerHTML = styleCode;
   container.appendChild(styleContainer);
 }
-
 function indicatorsHandler(e) {
   let target = e.target;
-
   if (target.classList.contains("indicators__item")) {
     target.style.backgroundColor = "red";
-
     if (prevIndicator !== null) prevIndicator.removeAttribute("style");
-
     prevIndicator = target;
   }
 }
-
 function setListener() {
   let indicatorsContainer = document.querySelector("div.indicators");
-
   indicatorsContainer.addEventListener("click", indicatorsHandler);
 }
-
 function createCarousel(slidesCount = 5) {
   // createContainer();
   container = document.querySelector("#carousel");
@@ -131,5 +114,4 @@ function createCarousel(slidesCount = 5) {
   createStyle();
   setListener();
 }
-
 createCarousel();
